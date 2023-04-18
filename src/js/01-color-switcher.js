15bd7ed322606ca7090472975c0dde1067148a29
Dmytro Berezhnoi <berezhnoi.dmytro@gmail.com>
2023-04-18 13:49:59 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const buttonStartEl = document.querySelector('[data-start]');
+const buttonStopEl = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
 
+buttonStartEl.addEventListener('click', changeColors);
+buttonStopEl.addEventListener('click', stopChangeColors);
+let idInterval = null;
+
+function changeColors() {
+  idInterval = setInterval(() => {
+    let randomColor = getRandomHexColor();
+
+    bodyEl.style.backgroundColor = randomColor;
+  }, 1000);
+
+  buttonStartEl.disabled = true;
+  buttonStopEl.disabled = false;
+}
+
+function stopChangeColors() {
+  clearInterval(idInterval);
+  buttonStartEl.disabled = false;
+  buttonStopEl.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
